99c86015cc8faa7bf3139fc868bb1adbd3862d17
indhuramesh <indhu131003@gmail.com>
2023-02-21 20:56:49 +0530

    first commit

--- project3/src/main/java/com/example/demo/controller/controller.java
+++ project3/src/main/java/com/example/demo/controller/controller.java
@@ -1,16 +1,15 @@
 package com.example.demo.controller;
-
-import java.util.Arrays;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.example.demo.model.pojoclass;
 @RestController
 public class controller {
-	@GetMapping("student")
-	public List <pojoclass> info(){
-		return Arrays.asList(new pojoclass(1,"indhu","IT"),new pojoclass(2,"ligi","IT"));
+	@Value("${value}")
+	private String name;
+	
+	@GetMapping("/")
+	public String display() {
+		return "Welcome to this "+name;
 	}
-
+	
 }
